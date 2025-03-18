8ecc22aa794f092b803327616e765caab34dfdd6
Ngochuy2137 <huy.nguyenngoc2137@gmail.com>
2025-03-18 15:33:15 +0900

    Fix heading angle of the robot
    Signed-off-by: Ngochuy2137 <huy.nguyenngoc2137@gmail.com>

--- src/trajectory_simulation.py
+++ src/trajectory_simulation.py
@@ -2,14 +2,23 @@ import rospy
 from geometry_msgs.msg import PoseStamped
 from nae_static.utils.submodules.training_utils.data_loader import DataLoader as NAEDataLoader
 import os
-from python_utils.printer import Printer
 from tqdm import tqdm
+import random
+import math
+from python_utils.printer import Printer
+from python_utils.plotter import Plotter
+from utils.utils import reset_robot, spawn_marker
 
 global_printer = Printer()
+global_plotter = Plotter()
+
+MAX_CATCH_DIST = 0.6
+DATA_WITH_Y_UP = False
 
 data_mother_dir = os.getenv('NAE_DATASET20')
 object_name = 'ball'
 data_dir = os.path.join(data_mother_dir, object_name, '3-data-augmented', 'data_plit')
+
 
 def publish_trajectories(data):
     rospy.init_node('pose_publisher', anonymous=True)
@@ -19,8 +28,23 @@ def publish_trajectories(data):
     while not rospy.is_shutdown():
         for traj_idx, traj in enumerate(data):
             global_printer.print_green(f'\nPublishing trajectory {traj_idx}. ENTER to continue...')
+            impact_point = traj[-1]
+            dist_random_x = random.uniform(-MAX_CATCH_DIST, MAX_CATCH_DIST)
+            dist_random_y = math.sqrt(MAX_CATCH_DIST**2 - dist_random_x**2)
+            if DATA_WITH_Y_UP:
+                impact_point_z_up = [impact_point[0], -impact_point[2], impact_point[1]]
+            else:
+                impact_point_z_up = impact_point
+            init_robot_pos = (impact_point_z_up[0]+dist_random_x, impact_point_z_up[1]+dist_random_y, 0.45)
+            # calculate orientation of robot to head to impact point
+            angle = math.atan2(-dist_random_y, -dist_random_x)
+
+            global_printer.print_yellow(f'  ENTER to reset robot to initial position: {init_robot_pos[0]}, {init_robot_pos[1]}'); input()
             print('     Data points: ', len(traj))
-            print('     Estimated time: ', len(traj)/120); input()
+            print('     Estimated time: ', len(traj)/120)
+            print('     impact_point: ', impact_point)
+            reset_robot(init_robot_pos[0], init_robot_pos[1], init_robot_pos[2], 0, 0, angle)
+            spawn_marker(impact_point_z_up[0], impact_point_z_up[1], impact_point_z_up[2])
 
             for point in tqdm(traj):
                 if point.shape[0] != 3:

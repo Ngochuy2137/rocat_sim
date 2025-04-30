a4d414389fe2f2440f9d5f6bdd3ac673b9ed3343
Ngochuy2137 <huy.nguyenngoc2137@gmail.com>
2025-04-30 17:58:13 +0900

    Debug impact checker error
    Signed-off-by: Ngochuy2137 <huy.nguyenngoc2137@gmail.com>

--- src/impact_checker.py
+++ src/impact_checker.py
@@ -6,10 +6,12 @@ from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Point, PoseStamped
 from python_utils.printer import Printer
 from std_srvs.srv import SetBool, SetBoolResponse
-from rocat_sim.src.utils.utils import Config
+from rocat_sim.src.utils.utils import Config, warn_beep
 import os
 import rospkg
 import numpy as np
+import datetime
+import threading
 
 # Load global config
 rospack = rospkg.RosPack()
@@ -21,12 +23,30 @@ GLOBAL_CONFIG = Config(json_file_path)
 global_printer = Printer()
 
 # Ngưỡng sai số để kiểm tra va chạm
-TOLERANCE_XY_LIST = GLOBAL_CONFIG.controller_tolerance_xy
-CATCHING_HEIGHT = GLOBAL_CONFIG.catching_height
+TOLERANCE_XY_LIST = np.array(GLOBAL_CONFIG.controller_tolerance_xy)
+# CATCHING_HEIGHT = GLOBAL_CONFIG.catching_height
 
-count = 0
+
+def shutdown_node():
+    rospy.loginfo("Shutting down the node...")
+    rospy.signal_shutdown("User requested shutdown")
+
+def wait_for_param(param_name, timeout=5.0):
+    start_time = rospy.get_time()
+    rate = rospy.Rate(10)  # 10 Hz
+
+    while not rospy.is_shutdown():
+        if rospy.has_param(param_name):
+            return rospy.get_param(param_name)
+
+        if rospy.get_time() - start_time > timeout:
+            raise TimeoutError(f"Timeout waiting for param: {param_name}")
+
+        rate.sleep()
+    return None
+
 class ImpactChecker:
-    def __init__(self):
+    def __init__(self, rate_hz=240):
         rospy.init_node("impact_checker", anonymous=True)
 
         # Subscribe vào topic vị trí robot (Odometry) và vật thể (Point)
@@ -36,101 +56,119 @@ class ImpactChecker:
         self.trial_count = 0
         # self.success_count = 0
 
-        rospy.Service('/reset_impact_checker_srv', SetBool, self.handle_reset_request)
-        rospy.Service('/ask_if_impact_checker_is_reset_srv', SetBool, self.handle_if_is_reset_ask)
+        rospy.Service('/trigger_impact_checker_srv', SetBool, self.handle_trigger_req)
+
         rospy.loginfo("ImpactChecker Service Server is running...")
 
         self.robot_position = None
+        self.success_matrix = []
         self.reset()
+
+        now = datetime.datetime.now()
+        self.time_start = now.strftime("%d-%m-%Y_%H-%M-%S")
+
+        self.log = ''
+        self.trigger_evt = threading.Event()
+        self.rate_hz = rate_hz
+        self.rate = rospy.Rate(rate_hz)
 
     def reset(self,):
-        self.success_matrix = []
-        self.this_trial_result = [] # [False] * len(TOLERANCE_XY_LIST)
         self.object_position = None
+        self.hello_object = False
+        self.hello_robot = False
     
-    def is_reset(self):
-        if len(self.success_matrix) == 0 and len(self.this_trial_result) == 0:
-            return True
-        else:
-            print('Not reset yet:')
-            print(f'     self.success_matrix:       {len(self.success_matrix)}')
-            print(f'     self.this_trial_result:    {len(self.this_trial_result)}')
-            print(f'     self.object_position:      {self.object_position}')
-            return False
-
-    def handle_reset_request(self, req):
-        global_printer.print_green("Received service RESET REQUEST /reset_impact_checker_srv")
-        """Xử lý service request để reset self.trial_count."""
-        if len(self.this_trial_result) == 0:
-            return SetBoolResponse(success=True, message="Counter reset to 0")
-        
-        self.trial_count += 1
-        global_printer.print_green(f"\n{'-'*25} ✅ New catching {self.trial_count} {'-'*25}")
-
-        # tổng hợp kết quả
-        self.this_trial_result = np.array(self.this_trial_result)
-        self.this_trial_result = np.sum(self.this_trial_result, axis=0, dtype=bool)
-        if self.this_trial_result.shape[0] != len(TOLERANCE_XY_LIST):
-            raise ValueError(f"self.this_trial_result shape {self.this_trial_result.shape} is not equal to len(TOLERANCE_XY_LIST) {len(TOLERANCE_XY_LIST)}")
-
-        print(f"       Trial {self.trial_count} result: {self.this_trial_result}")
-        self.success_matrix.append(self.this_trial_result)
-
-        # print percentage of self.success_matrix
-        success_matrix_np = np.array(self.success_matrix)
-        success_percentage = np.sum(success_matrix_np, axis=0) / success_matrix_np.shape[0]
-        if success_percentage.shape[0] != len(TOLERANCE_XY_LIST):
-            raise ValueError(f"success_percentage shape {success_percentage.shape} is not equal to len(TOLERANCE_XY_LIST) {len(TOLERANCE_XY_LIST)}")
-        print(f"       Success percentage: {success_percentage}")
-
-        # reset 
+    def handle_trigger_req(self, req):
+        global_printer.print_green("1. Got trigger request")
         self.reset()
-        
-        return SetBoolResponse(success=True, message="Counter reset to 0")
-    
-    def handle_if_is_reset_ask(self, req):
-        done_reset = self.is_reset()
-        global_printer.print_green(f"Received service QUESTION /ask_if_impact_checker_is_reset_srv -> {done_reset}")
-        if done_reset:
-            return SetBoolResponse(success=True, message="Impact checker has been reset")
-        else:
-            return SetBoolResponse(success=False, message="Impact checker has not been reset yet")
+        self.trigger_evt.set()
 
+        return SetBoolResponse(success=True, message="Reset and ready to check impact")
+    
     def robot_callback(self, msg):
         """Hàm callback nhận dữ liệu từ topic Odometry."""
         self.robot_position = [msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z]
-        self.check_impact()
+        self.hello_robot = True
 
     def object_callback(self, msg:PoseStamped):
         """Hàm callback nhận dữ liệu từ topic chứa vị trí vật thể."""
         self.object_position = [msg.pose.position.x, msg.pose.position.y, msg.pose.position.z]
-        self.check_impact()
+        self.hello_object = True
 
-    def check_impact(self):
-        # global count; print(f'Count: {count} - {self.trial_count}'); count += 1
-        """Hàm kiểm tra xem robot có đến gần vật thể trong khoảng sai số không."""
-        if self.robot_position is None or self.object_position is None:
-            return  # Chưa có đủ thông tin
-        
-        if abs(self.object_position[2] - CATCHING_HEIGHT) > 0.02:
-            return
-        
-        robot_x, robot_y, _ = self.robot_position
-        object_x, object_y, _ = self.object_position
+    def check_impact(self, real_catching_point_with_z_up):
+        while not rospy.is_shutdown():
+            if self.object_position is None:
+                self.log = 'Waiting for object position'
+                self.rate.sleep()
+                continue
+            if self.robot_position is None:
+                self.log = 'Waiting for robot position'
+                self.rate.sleep()
+                continue
 
-        # Tính khoảng cách giữa robot và vật thể
-        distance_xy = math.sqrt((robot_x - object_x) ** 2 + (robot_y - object_y) ** 2)
+            time_start = rospy.Time.now().to_sec()
+            """Hàm kiểm tra xem robot có đến gần vật thể trong khoảng sai số không."""
+            real_catching_point_with_z_up = np.array(real_catching_point_with_z_up)
+            if np.allclose(self.object_position, real_catching_point_with_z_up, atol=0.01):
+                self.log = '3'
+                global_printer.print_green(f'Detected catching point')
 
-        this_loop_result = [False] * len(TOLERANCE_XY_LIST)
-        for i, tolerance in enumerate(TOLERANCE_XY_LIST):
-            if distance_xy <= tolerance:
-                this_loop_result[i] = True
-        
-        self.this_trial_result.append(this_loop_result)
+                # 1. Check kết quả của lần thử nghiệm này
+                robot_xy = np.array(self.robot_position)
+                object_xy = np.array(self.object_position)
+                # Tính khoảng cách giữa robot và vật thể
+                dis_xy = np.linalg.norm(robot_xy - object_xy)
+                this_trial_result = dis_xy <= np.array(TOLERANCE_XY_LIST)
+
+                if rospy.Time.now().to_sec()- time_start > 1/self.rate_hz:
+                    global_printer.print_purple(f"       Checking takes so long time: {rospy.Time.now().to_sec()- time_start}")
+                
+                print(f'This trial result: {this_trial_result}')
+                self.success_matrix.append(this_trial_result)
+
+
+                # 2. Check kết quả của tất cả các lần thử nghiệm
+                success_matrix_np = np.array(self.success_matrix)
+                success_percentage = np.sum(success_matrix_np, axis=0) / success_matrix_np.shape[0]
+                assert success_percentage.shape[0] == len(TOLERANCE_XY_LIST)
+                print(f"       Success percentage: {success_percentage}")
+                
+                # 3. Ghi file
+                try:
+                    result_folder = os.path.join(package_path, 'results')
+                    os.makedirs(result_folder, exist_ok=True)
+                    file_path = os.path.join(result_folder, f'impact_checker-{self.time_start}.txt')
+                    with open(file_path, 'a') as f:
+                        f.write(f"Trial {self.trial_count} result: {this_trial_result}\n")
+                        f.write(f"Success percentage: {success_percentage}\n")
+                        f.write(f"{'-'*50}\n")
+                    print(f"       Result saved to {file_path}")
+                except Exception as e:
+                    rospy.logerr(f"Error when logging to file: {e}")
+                return
+                    
+            self.rate.sleep()
+
+    def run(self,):
+        """Hàm chạy vòng lặp chính của node."""
+        while not rospy.is_shutdown():
+            if self.trigger_evt.wait():
+                global_printer.print_blue(f"\n{'='*25} TRIAL #{self.trial_count} {'='*25}", background=True)
+                self.trial_count += 1
+                # wait awhile
+                rospy.sleep(1)
+                # update catching height and number of trajectory points
+                real_catching_point_with_z_up = rospy.get_param('/real_catching_point_with_z_up')
+                print(f'Updated new trajectory params:')
+                print(f'    Real catching point (with z up):    {real_catching_point_with_z_up}')
+                print(f'    Catching height:                    {real_catching_point_with_z_up[-1]}')
+                
+                self.check_impact(real_catching_point_with_z_up)
+                self.trigger_evt.clear()
 
 if __name__ == "__main__":
     try:
-        ImpactChecker()
-        rospy.spin()
+        ic = ImpactChecker()
+        # rospy.spin()
+        ic.run()
     except rospy.ROSInterruptException:
         pass
--- src/throw_manager_node.py
+++ src/throw_manager_node.py
@@ -1,3 +1,5 @@
+#!/home/huynn/anaconda3/envs/nae-dynamic-3-pc/bin/python
+
 import rospy
 import rospkg
 import os
@@ -21,6 +23,8 @@ from rocat_sim.src.utils.utils import (
     Config,
     warn_beep
 )
+
+# from rocat_sim.srv import UpdateCatchingHeight, UpdateCatchingHeightRequest
 
 def shutdown_node():
     rospy.loginfo("Shutting down the node...")
@@ -50,54 +54,49 @@ class ThrowManager:
         # Publishers
         self.traj_pub = rospy.Publisher(f'/mocap_sim/ball', PoseStamped, queue_size=10)
         self.marker_pub = rospy.Publisher(self.config.real_trajectory_topic, Marker, queue_size=10)
-        self.trigger_pub = rospy.Publisher(self.config.trigger_topic, PoseStamped, queue_size=100)
+        self.go1_trigger_pub = rospy.Publisher(self.config.trigger_topic, PoseStamped, queue_size=100)
         self.object_pub = rospy.Publisher(self.config.realtime_object_pose_topic, PoseStamped, queue_size=10)
 
         # Service server
         # Service from robot controller node
         rospy.Service('/robot_reached_goal_srv', SetBool, self.handle_robot_reach_goal_srv)
 
+        if rospy.has_param('/catching_height'):
+            rospy.delete_param('/catching_height')
+            print("Deleted param /catching_height")
+        else:
+            print("Param /catching_height does not exist")
+        
         # Service clients        
-        rospy.wait_for_service('/reset_impact_checker_srv', timeout=10)
-        self.reset_impact_checker_client = rospy.ServiceProxy('/reset_impact_checker_srv', SetBool)
+        # 1. Impact checker
+        rospy.wait_for_service('/trigger_impact_checker_srv', timeout=10)
+        self.trigger_impact_checker_client = rospy.ServiceProxy('/trigger_impact_checker_srv', SetBool)
 
-        rospy.wait_for_service('/ask_if_impact_checker_is_reset_srv', timeout=5)
-        self.ask_impact_checker_client = rospy.ServiceProxy('/ask_if_impact_checker_is_reset_srv', SetBool)
-
+        # 2. Robot controller
         rospy.wait_for_service('/ask_if_robot_is_free_srv', timeout=10)
         self.ask_robot_controller_client = rospy.ServiceProxy('/ask_if_robot_is_free_srv', SetBool)
 
+        # 3. NAE predictor
         rospy.wait_for_service('NAE/trigger_new_prediction', timeout=10)
         self.trigger_nae_predictor_client = rospy.ServiceProxy('NAE/trigger_new_prediction', SetBool)
 
-    def send_reset_impact_checker_srv(self, ):
-        """Call the reset service to trigger the impact checker reset."""
-        global_printer.print_green("Sending reset signal to impact checker...")
+        self.already_asked_last_result = False # this var is to guarantee that asking for last result before reset the impact checker
+
+    def send_trigger_impact_checker_srv(self, ):
+        """Call the trigger service to trigger the impact checker trigger."""
+        global_printer.print_green("-> IMPACT CHECKER: Sending trigger signal to impact checker...")
         try:
             req = SetBoolRequest(data=True)
-            resp = self.reset_impact_checker_client(req)
-            if resp.success:
-                global_printer.print_green(f"   Reset response: success={resp.success}, message='{resp.message}'")
-            else:
-                global_printer.print_red(f"     Reset response: success={resp.success}, message='{resp.message}'")
-        except rospy.ServiceException as e:
-            rospy.logerr(f"Reset service call failed: {e}")
-
-    def send_ask_impact_checker_srv(self):
-        """Call the ask service to check if the impact checker is reset."""
-        global_printer.print_green("Asking if impact checker is reset...")
-        try:
-            req = SetBoolRequest(data=False)
-            resp = self.ask_impact_checker_client(req)
-            print(f"        Ask response: success={resp.success}, message='{resp.message}'")
+            resp = self.trigger_impact_checker_client(req)
             return resp.success
+
         except rospy.ServiceException as e:
-            global_printer.print_red(f"     Ask impact checker failed: {e}")
+            rospy.logerr(f"trigger service call failed: {e}")
             return False
-        
+
     def send_ask_robot_srv(self):
         """Call the ask service to check if the robot is free."""
-        global_printer.print_green("Asking if robot is free...")
+        global_printer.print_green("-> ROBOT CONTROLLER: Asking if robot is free...")
         try:
             req = SetBoolRequest(data=False)
             resp = self.ask_robot_controller_client(req)
@@ -109,7 +108,7 @@ class ThrowManager:
         
     def send_trigger_nae_predictor_srv(self):
         """Call the trigger service to trigger the NAE predictor."""
-        global_printer.print_green("Asking if NAE predictor is ready for new prediction ...")
+        global_printer.print_green("-> NAE PREDICTOR: Asking if NAE predictor is ready for new prediction ...")
         try:
             req = SetBoolRequest(data=False)
             resp = self.trigger_nae_predictor_client(req)
@@ -118,6 +117,7 @@ class ThrowManager:
         except rospy.ServiceException as e:
             global_printer.print_red(f"     Ask NAE predictor failed: {e}")
             return False
+    
         
     def handle_robot_reach_goal_srv(self, req: SetBoolRequest) -> SetBoolResponse:
         """Service callback resetting the impact checker when robot reaches goal."""
@@ -128,64 +128,71 @@ class ThrowManager:
             shutdown_node()
         
         global_printer.print_green("        Received INFO robot reach goal signal")
-        self.send_reset_impact_checker_srv()
+        rospy.sleep(1)
         return SetBoolResponse(success=True, message="Thank you for the signal")
 
     def publish_trajectories(self):
-        # Initial reset
-        self.send_reset_impact_checker_srv()
-
-        for traj_idx, traj in enumerate(self.data):
+        n = len(self.data)
+        trial_num_target = max(n, 100)
+        # for traj_idx, traj in enumerate(self.data):
+        for traj_idx in range(trial_num_target):
+            traj = self.data[traj_idx % n]
             if rospy.is_shutdown():
                 break
-            # ask for init state before processing new trajectory
-            while not self.send_ask_impact_checker_srv() or not self.send_ask_robot_srv() or not self.send_trigger_nae_predictor_srv():
-                global_printer.print_yellow("Waiting for: ")
-                print(f"        - Impact checker reset")
-                print(f"        - Robot controller free")
-                print(f"        - NAE predictor ready for new prediction")
-                rospy.sleep(1)
 
-
-            global_printer.print_green(f"\n{'-'*25} Ready to publish trajectory {traj_idx} {'-'*25}", background=True)
-
-            # Validate trajectory shape
+            # 1. Check trajectory shape
             if traj.shape[1] != 4:
                 raise ValueError('Trajectory point must have 4 dimensions (t, x, y, z)')
-
-            # cut so that last point is close to catching_height
-            index = np.abs(traj[:, 2] - self.config.catching_height).argmin()
-            if abs(traj[index, 2] - self.config.catching_height) > 0.05:
-                global_printer.print_red(f"WARNING: Trajectory {traj_idx} is not close to catching height")
-            traj = traj[:index + 1, :]
-
+            
+            # 2. update param server
             # Compute impact point
-            impact_no_t = traj[-1, 1:]
             if self.DATA_WITH_Y_UP:
-                impact_up = [impact_no_t[0], -impact_no_t[2], impact_no_t[1]]
+                real_catching_point_with_z_up = [traj[-1, 1], -traj[-1, 3], traj[-1, 2]]
             else:
-                impact_up = impact_no_t
+                real_catching_point_with_z_up = [traj[-1, 1], traj[-1, 2], -traj[-1, 3]]
+            real_catching_point_with_z_up = [float(x) for x in real_catching_point_with_z_up]
+            catching_height = real_catching_point_with_z_up[2]
+            rospy.set_param('/catching_height', catching_height)    # height is y axis in this case
+            rospy.set_param('/real_catching_point_with_z_up', real_catching_point_with_z_up)
 
-            # Determine robot initial position
+            # 3. Check if components are ready
+            while not self.send_ask_robot_srv():
+                global_printer.print_yellow("       Waiting for Robot controller free")
+                rospy.sleep(1)
+            while not self.send_trigger_nae_predictor_srv():
+                global_printer.print_yellow("       Waiting for NAE predictor ready for new prediction")
+                rospy.sleep(1)
+            while not self.send_trigger_impact_checker_srv():
+                global_printer.print_yellow("       Waiting for Impact checker reset")
+                rospy.sleep(1)
+
+            global_printer.print_blue(f"\n{'='*25} TRIAL #{traj_idx} {'='*25}", background=True)
+            print(f'    Updated new catching height {catching_height} ->')
+            # input('Press ENTER to continue to next trajectory')
+
+            # 4. Set robot to initial position
+            # Calculate robot initial position
             alpha = random.uniform(-self.config.rocat_sim_conf.catch_ori_dev_deg_thres,
                                    self.config.rocat_sim_conf.catch_ori_dev_deg_thres)
-            init_pos = find_point_A(impact_up[0], impact_up[1], alpha_degree=alpha,
+            init_pos = find_point_A(real_catching_point_with_z_up[0], real_catching_point_with_z_up[1], alpha_degree=alpha,
                                     d=self.config.rocat_sim_conf.catch_dist)
 
-            input(f"Press ENTER to reset robot to init position {init_pos}")
+            # input(f"Press ENTER to reset robot to init position {init_pos}")
+            # wait for second before new run
+            rospy.sleep(5)
+            # Reset robot to initial position
             reset_robot(x_init=init_pos[0], y_init=init_pos[1])
 
+            # 5. Visualization
             # Prepare visualization markers for trajectory
             if self.DATA_WITH_Y_UP:
                 traj_vis = np.array([[p[1], -p[3], p[2]] for p in traj])
             else:
                 traj_vis = traj[:, 1:]
-
             publish_marker_list_2gzb(traj_vis, model_name="real_IP", color="green")
             publish_points(points_pub=self.marker_pub, points=traj_vis)
-
             # Delay before trigger
-            global_printer.print_green('Waiting 2 seconds before triggering...')
+            global_printer.print_green('Waiting 2 seconds before triggering controller ...')
             rospy.sleep(2)
 
             # Trigger robot catch
@@ -193,8 +200,8 @@ class ThrowManager:
             pose.header.stamp = rospy.Time.now()
             pose.header.frame_id = 'world'
             pose.pose.orientation.w = 1.0
-            self.trigger_pub.publish(pose)
-            rospy.sleep(self.config.rocat_sim_conf.trigger_n_thow_time_gap_sim)
+            self.go1_trigger_pub.publish(pose)
+            rospy.sleep(self.config.rocat_sim_conf.trigger_n_thow_time_gap_sim) # sleep awhile after trigger
 
             # Publish trajectory points in real-time
             rate = rospy.Rate(120)

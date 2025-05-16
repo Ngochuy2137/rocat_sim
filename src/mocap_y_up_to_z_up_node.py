66ca2bf3f13a834b41ec5379b2e2fd84a3c4c580
Ngochuy2137 <huy.nguyenngoc2137@gmail.com>
2025-05-16 09:51:01 +0900

    mocap_y_up_to_z_up_node.py -> pub robot pose with odometry msg
    Signed-off-by: Ngochuy2137 <huy.nguyenngoc2137@gmail.com>

--- src/mocap_y_up_to_z_up_node.py
+++ src/mocap_y_up_to_z_up_node.py
@@ -2,56 +2,74 @@
 import rospy
 import math
 from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Odometry
 import tf.transformations as tft
 
-class PoseConverter:
-    def __init__(self):
-        # --- 1) Đọc tham số (private) hoặc dùng mặc định ---
-        self.robot_in_topic  = rospy.get_param('robot_pose_y_up_topic')
-        self.robot_out_topic = rospy.get_param('robot_pose_z_up_topic')
-        self.obj_in_topic    = rospy.get_param('object_pose_y_up_topic')
-        self.obj_out_topic   = rospy.get_param('object_pose_z_up_topic')
+def pose_callback(msg: PoseStamped, args):
+    object_type, pub = args  # unpack tuple
 
-        # --- 2) Tạo 2 publisher ---
-        self.pub_robot = rospy.Publisher(self.robot_out_topic,  PoseStamped, queue_size=1)
-        self.pub_obj   = rospy.Publisher(self.obj_out_topic,    PoseStamped, queue_size=1)
+    # --- Chuyển đổi vị trí ---
+    x_old, y_old, z_old = msg.pose.position.x, msg.pose.position.y, msg.pose.position.z
+    x_new = x_old
+    y_new = -z_old
+    z_new = y_old
 
-        # --- 3) Tạo 2 subscriber và gán callback chung, truyền kèm publisher tương ứng ---
-        rospy.Subscriber(self.robot_in_topic, PoseStamped, self.callback, callback_args=self.pub_robot, queue_size=1)
-        rospy.Subscriber(self.obj_in_topic,   PoseStamped, self.callback, callback_args=self.pub_obj,   queue_size=1)
+    # --- Chuyển orientation ---
+    q_old = [
+        msg.pose.orientation.x,
+        msg.pose.orientation.y,
+        msg.pose.orientation.z,
+        msg.pose.orientation.w
+    ]
+    q_rot = tft.quaternion_from_euler(math.pi/2, 0.0, 0.0)
+    q_new = tft.quaternion_multiply(q_rot, q_old)
 
-        rospy.loginfo(f"[PoseConverter] Subscribed: robot⇨{self.robot_in_topic}, object⇨{self.obj_in_topic}")
-        rospy.loginfo(f"[PoseConverter] Publishing z-up: robot⇨{self.robot_out_topic}, object⇨{self.obj_out_topic}")
+    if object_type == "robot":
+        odom_msg = Odometry()
+        odom_msg.header = msg.header
+        odom_msg.header.frame_id = "world"  # hoặc "world" nếu cần
+        odom_msg.pose.pose.position.x = x_new
+        odom_msg.pose.pose.position.y = y_new
+        odom_msg.pose.pose.position.z = z_new
+        odom_msg.pose.pose.orientation.x = q_new[0]
+        odom_msg.pose.pose.orientation.y = q_new[1]
+        odom_msg.pose.pose.orientation.z = q_new[2]
+        odom_msg.pose.pose.orientation.w = q_new[3]
+        pub.publish(odom_msg)
 
-    def callback(self, msg: PoseStamped, pub):
-        # --- chuyển vị trí ---
-        x, y, z = msg.pose.position.x, msg.pose.position.y, msg.pose.position.z
-        new_pos = ( x,     -z,     y )
+    elif object_type == "flying_object":
+        pose_msg = PoseStamped()
+        pose_msg.header = msg.header
+        pose_msg.pose.position.x = x_new
+        pose_msg.pose.position.y = y_new
+        pose_msg.pose.position.z = z_new
+        pose_msg.pose.orientation.x = q_new[0]
+        pose_msg.pose.orientation.y = q_new[1]
+        pose_msg.pose.orientation.z = q_new[2]
+        pose_msg.pose.orientation.w = q_new[3]
+        pub.publish(pose_msg)
 
-        # --- chuyển quaternion (xoay +90° quanh trục x) ---
-        q_old = [
-            msg.pose.orientation.x,
-            msg.pose.orientation.y,
-            msg.pose.orientation.z,
-            msg.pose.orientation.w
-        ]
-        q_rot = tft.quaternion_from_euler(math.pi/2, 0.0, 0.0)
-        q_new = tft.quaternion_multiply(q_rot, q_old)
-
-        # --- đóng gói PoseStamped mới và publish ---
-        out = PoseStamped()
-        out.header        = msg.header
-        out.pose.position.x = new_pos[0]
-        out.pose.position.y = new_pos[1]
-        out.pose.position.z = new_pos[2]
-        out.pose.orientation.x = q_new[0]
-        out.pose.orientation.y = q_new[1]
-        out.pose.orientation.z = q_new[2]
-        out.pose.orientation.w = q_new[3]
-
-        pub.publish(out)
-
+# ----------------------
 if __name__ == '__main__':
     rospy.init_node('mocap_y_up_to_z_up_node')
-    converter = PoseConverter()
+
+    robot_in_topic  = rospy.get_param('robot_pose_y_up_topic')
+    robot_out_topic = rospy.get_param('robot_pose_z_up_topic')
+    obj_in_topic    = rospy.get_param('object_pose_y_up_topic')
+    obj_out_topic   = rospy.get_param('object_pose_z_up_topic')
+    
+    pub_robot = rospy.Publisher(robot_out_topic, Odometry, queue_size=10)
+    pub_object = rospy.Publisher(obj_out_topic, PoseStamped, queue_size=10)
+
+    rospy.Subscriber(robot_in_topic,
+                     PoseStamped,
+                     pose_callback,
+                     callback_args=("robot", pub_robot))
+
+    rospy.Subscriber(obj_in_topic,
+                     PoseStamped,
+                     pose_callback,
+                     callback_args=("flying_object", pub_object))
+
+    rospy.loginfo("[PoseConverter] Running... robot → Odometry | flying object → PoseStamped")
     rospy.spin()

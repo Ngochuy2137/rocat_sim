2ab50c5066d5fd6e37df29a8296fd71b932ef94b
Ngochuy2137 <huy.nguyenngoc2137@gmail.com>
2025-06-22 05:26:12 +0900

    commit update in throw manage, config
    Signed-off-by: Ngochuy2137 <huy.nguyenngoc2137@gmail.com>

--- src/throw_manager_node.py
+++ src/throw_manager_node.py
@@ -352,7 +352,7 @@ if __name__ == '__main__':
 
     all_objects_list = [
         'ball', 'big_sized_plane', 'boomerang', 'cardboard', 'ring_frisbee',
-        'cookie_box', 'water_bottle', 'paper_cup', 'noodle_cup', 'cap'
+        # 'cookie_box', 'water_bottle', 'paper_cup', 'noodle_cup', 'cap'
     ]
     trial_num_target_per_obj = 100
     manager = ThrowManager()

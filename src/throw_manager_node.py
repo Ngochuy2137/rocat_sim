d86b95eab46cc798db1059e1a98faeb8d443170a
Ngochuy2137 <huy.nguyenngoc2137@gmail.com>
2025-07-02 01:39:19 +0900

    manager -> update seen and unseen objects (new, after adding pinwheel, small_sized_plane
    Signed-off-by: Ngochuy2137 <huy.nguyenngoc2137@gmail.com>

--- src/throw_manager_node.py
+++ src/throw_manager_node.py
@@ -378,9 +378,8 @@ if __name__ == '__main__':
       # cardboard         -> 
       # ring_frisbee      -> 
 
-    # all_objects_list = ['boomerang', 'big_sized_plane', 'carpet', 'hat', 'ring_frisbee']
-    all_objects_list = ['sand_can']
-
+    all_objects_list = ['boomerang', 'big_sized_plane', 'carpet', 'hat', 'ring_frisbee',
+                        'cookie_box', 'noodle_cup', 'paper_cup', 'pinwheel', 'small_sized_plane',]
     trial_num_target_per_obj = 100
     manager = ThrowManager()
     for object_name in all_objects_list:
